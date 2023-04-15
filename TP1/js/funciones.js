9533bfa82130b316e0057027876341ef3ff551ac
Estebancopello <129554050+Estebancopello@users.noreply.github.com>
2023-04-14 21:33:58 -0300

    Add files via upload

---
+++ TP1/js/funciones.js
@@ -0,0 +1,6 @@
+function mifuncion(){
+    const aux=document.getElementById("c1");
+    const aux2=document.getElementById("c2");
+    const aux3=document.getElementById("c3");
+    alert("Nombre: "+ aux.value + ", " + "Apellido: " + aux2.value + ", " + "Años: " + aux3.value);
+}
